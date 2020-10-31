e1b9965374c11c5f9e17898d9e5f3520e8a0293b
Elvira Semenova <shalopayka@gmail.com>
2020-10-31 15:47:59 +0300

    files added

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 236320;
+        int bonusDivider = 20;
+        int bonusMile = ticketPrice/bonusDivider;
+        System.out.println(bonusMile);
+    }
+}
